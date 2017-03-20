c658fe321ff33971cb7d07635cbcc008b20bef68
Peter Kang <peter.kang@cmlmediasoft.local>
2017-03-20 09:46:10 -0700

    files pushed

---
+++ slider.js
@@ -0,0 +1,44 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 200,
+    loop:true,
+    margin:0,
+    items:1,
+    nav:true,
+    navText: [
+        "<i class='fa fa-caret-left'></i>",
+        "<i class='fa fa-caret-right'></i>"
+    ],
+    dots:true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1,
+            stagePadding: 60
+        },
+        600:{
+            items:1,
+            stagePadding: 100
+        },
+        1000:{
+            items:1,
+            stagePadding: 200
+        },
+        1200:{
+            items:1,
+            stagePadding: 250
+        },
+        1400:{
+            items:1,
+            stagePadding: 300
+        },
+        1600:{
+            items:1,
+            stagePadding: 350
+        },
+        1800:{
+            items:1,
+            stagePadding: 400
+        }
+    }
+});
